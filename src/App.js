51de19da2174b7910addc9a609f2f53800c5e4cc
nishadmuhammad <nishad@mapletechspace.ca>
2023-03-03 20:38:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './todo-app/home';
 import Todo from './MyTodo/mitodo';
-import Example from './example';
 
 
 
 function App() {
   return (
     <div className="App">
-    {/* <Todo/> */}
-    <Home></Home>
-    
+    <Todo/>
     </div>
   );
 }
